e680d6da7872896b339b876f87992e6b8bb32801
seanmx96 <seanmx96@users.noreply.github.com>
2020-06-12 10:05:41 -0500

    Created project skeleton and added interview answers.

---
+++ sprintchallenge/src/main/java/com/lambdaschool/sprintchallenge/SprintchallengeApplication.java
@@ -0,0 +1,16 @@
+package com.lambdaschool.sprintchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintchallengeApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SprintchallengeApplication.class,
+            args);
+    }
+
+}
